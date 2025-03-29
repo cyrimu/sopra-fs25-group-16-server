9d8587d1ba6cb9aea3179010f00997289f4b5f55
Piotr Wojtaszewski <piotrmaciej.wojtaszewski@uzh.ch>
2025-03-29 18:37:06 +0100

    first websocket connection

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic");
+	}
+}
