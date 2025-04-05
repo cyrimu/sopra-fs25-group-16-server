15bacc703b4c378df4e4616dbf49de321c5e8d67
Piotr Wojtaszewski <piotrmaciej.wojtaszewski@uzh.ch>
2025-04-05 17:45:53 +0200

    made create, update, join, leave lobby endpoints. Started on the LobbyService implementations. created new transfer objects for lobby and game updates & auxillary serializers

--- src/main/java/ch/uzh/ifi/hase/soprafs24/classes/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/classes/Lobby.java
@@ -4,25 +4,68 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import ch.uzh.ifi.hase.soprafs24.classes.Player;
+import ch.uzh.ifi.hase.soprafs24.constant.GameType;
+import ch.uzh.ifi.hase.soprafs24.constant.SupportedLanguages;
+
 public class Lobby {
-    final UUID mLobbyID;
+    public static final int ID_LENGTH = 8;
+
+    final String mLobbyID;
     final String mHost;
+    Player[] mPlayers;
+    GameType mType;
+    SupportedLanguages mLanguage;
+    Game mCurrentGame;
 
     // started implementing this because I need something so that i can start making the calls 
 
-    public Lobby() {
-        this.mLobbyID = UUID.randomUUID();
-        this.mHost = "Alice";
+    public Lobby(String host, Player[] players, GameType type, SupportedLanguages language) {
+        this.mLobbyID = UUID.randomUUID().toString().substring(0,ID_LENGTH);
+        this.mHost = host;
+        this.mPlayers = players;
+        this.mType = type;
+        this.mLanguage = language;
+        this.mCurrentGame = null;
+    }
+
+    public String getLobbyID() {
+        return mLobbyID;
     }
 
     public String getHost() {
         return mHost;
     }
 
-    // this will obviously be replaced
-    public List<String> getPlayers() {
-        List<String> players = new ArrayList<>();
-        players.add("Alice");
-        return players;
+    public GameType getGameType() {
+        return mType;
     }
+
+    public SupportedLanguages getLanguage() {
+        return mLanguage;
+    }
+
+    public Player[] getPlayers() {
+        return new Player[] {new Player(mPlayers[0]), new Player(mPlayers[1]), new Player(mPlayers[2]), new Player(mPlayers[3])};
+    }
+
+    public Game getCurrentGame() {
+        return mCurrentGame;
+    }
+
+    // set this when we start a game from this lobby
+    public void setCurrentGame(Game game) {
+        this.mCurrentGame = game;
+    }
+
+    public int getPlayerCount() {
+        int count = 0;
+        for (Player p : mPlayers) {
+            if (p != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RestGameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RestGameController.java
@@ -8,6 +8,7 @@ import ch.uzh.ifi.hase.soprafs24.constant.PlayerRoles;
 import ch.uzh.ifi.hase.soprafs24.constant.SupportedLanguages;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.GameConfigurationDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.PlayerDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.GameService;
 import org.springframework.http.HttpStatus;
@@ -63,13 +64,13 @@ public class RestGameController {
     // we use a custom conversion function because the normal DTO mapper is not equipped for this kind of objects
     private GameConfiguration convertGameConfigurationDTOtoGameConfiguration(GameConfigurationDTO dto) {
         GameConfiguration gameConfiguration = new GameConfiguration();
-        
-        List<String> playerNames = dto.getPlayerNames();
-        
-        Player[] players = new Player[] {   new Player(playerNames.get(0), PlayerRoles.BLUE_SPYMASTER), 
-                                            new Player(playerNames.get(1), PlayerRoles.BLUE_OPERATIVE), 
-                                            new Player(playerNames.get(2), PlayerRoles.RED_SPYMASTER), 
-                                            new Player(playerNames.get(3), PlayerRoles.RED_OPERATIVE)};
+
+        PlayerDTO[] playerDTOs = dto.getPlayers();
+        Player[] players = new Player[playerDTOs.length];
+
+        for (int i = 0; i < playerDTOs.length; i++) {
+            players[i] = new Player(playerDTOs[i].getPlayerName());
+        }
 
         gameConfiguration.setID(UUID.randomUUID().toString().substring(0, Game.ID_LENGTH));
         gameConfiguration.setHost(dto.getHost());
@@ -78,10 +79,10 @@ public class RestGameController {
         gameConfiguration.addPlayer(players[2]);
         gameConfiguration.addPlayer(players[3]);
 
-        gameConfiguration.setPlayerRole(playerNames.get(0), PlayerRoles.BLUE_SPYMASTER);
-        gameConfiguration.setPlayerRole(playerNames.get(1), PlayerRoles.BLUE_OPERATIVE);
-        gameConfiguration.setPlayerRole(playerNames.get(2), PlayerRoles.RED_SPYMASTER);
-        gameConfiguration.setPlayerRole(playerNames.get(3), PlayerRoles.RED_OPERATIVE);
+        gameConfiguration.setPlayerRole(players[0].getPlayerName(), PlayerRoles.BLUE_SPYMASTER);
+        gameConfiguration.setPlayerRole(players[1].getPlayerName(), PlayerRoles.BLUE_OPERATIVE);
+        gameConfiguration.setPlayerRole(players[2].getPlayerName(), PlayerRoles.RED_SPYMASTER);
+        gameConfiguration.setPlayerRole(players[3].getPlayerName(), PlayerRoles.RED_OPERATIVE);
         gameConfiguration.setType(dto.getGameType());
         gameConfiguration.setLanguage(dto.getLanguage());
         
